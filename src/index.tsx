dd6981c32ded25291bde099cc657bb9c61297f96
Sun ju Lee <yohekisa@gmail.com>
2020-05-29 17:34:28 +0900

    created inputbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
+// import "react-app-polyfill/ie11";
+// import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "app/view/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-

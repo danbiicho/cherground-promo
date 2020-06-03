f2fa3dc335a85816d4e5789643bbd9175bbe3f5f
Sun ju Lee <yohekisa@gmail.com>
2020-06-03 15:39:31 +0900

    added dropdown box

--- src/app/view/App.tsx
+++ src/app/view/App.tsx
@@ -5,6 +5,7 @@ import GlobalStyle from "app/view/style/GlobalStyle";
 import SignUpView from "app/view/components/signup-view/SignUpView";
 import SignUpViewPr from "app/view/components/practice-SignUp/SignUpViewPr";
 import LogInView from "app/view/components/login-view/LogInView";
+import DropDownBox from "app/view/widgets/DropDownBox";
 
 const App: React.FunctionComponent = () => {
   return (
@@ -13,8 +14,8 @@ const App: React.FunctionComponent = () => {
       <Switch>
         <Route exact path="/" component={SignUpView} />
         <Route path="/login" exact component={LogInView} />
-        <Route exact path="/signup" component={SignUpView} />
-        <Route exact path="/test" component={SignUpViewPr} />
+        <Route exact path="/signup" component={SignUpViewPr} />
+        <Route exact path="/test" component={DropDownBox} />
       </Switch>
     </BrowserRouter>
   );

7376b565fb5fed79691246e67d79b5bc8d86816f
Sun ju Lee <yohekisa@gmail.com>
2020-06-03 16:10:25 +0900

    added searchBox

--- src/app/view/App.tsx
+++ src/app/view/App.tsx
@@ -5,7 +5,8 @@ import GlobalStyle from "app/view/style/GlobalStyle";
 import SignUpView from "app/view/components/signup-view/SignUpView";
 import SignUpViewPr from "app/view/components/practice-SignUp/SignUpViewPr";
 import LogInView from "app/view/components/login-view/LogInView";
-import DropDownBox from "app/view/widgets/DropDownBox";
+//import DropDownBox from "app/view/widgets/DropDownBox";
+import SearchBox from "app/view/widgets/SearchBox";
 import LogInViewTest from "app/view/components/login-view/LogInViewTest";
 
 const App: React.FunctionComponent = () => {
@@ -16,7 +17,7 @@ const App: React.FunctionComponent = () => {
         <Route exact path="/" component={SignUpView} />
         <Route path="/login" exact component={LogInView} />
         <Route exact path="/signup" component={SignUpViewPr} />
-        <Route exact path="/test" component={DropDownBox} />
+        <Route exact path="/test" component={SearchBox} />
         <Route exact path="/logintest" component={LogInViewTest} />
       </Switch>
     </BrowserRouter>

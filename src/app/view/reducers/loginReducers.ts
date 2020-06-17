685ef8d50c3aea7c27a11f675c1bbc93e6b630d0
danbi <danbiicho@gmail.com>
2020-06-17 16:06:56 +0900

    login API

--- src/app/view/reducers/loginReducers.ts
+++ src/app/view/reducers/loginReducers.ts
@@ -3,19 +3,22 @@ const reducer = (state: React.ComponentState, action: any) => {
     case "ID_CHECK":
       return {
         ...state,
-        userName: action.value,
+        email: action.value,
       };
 
-    case "PASSWORDVALUE_CHECK":
+    case "PASSWORD_CHECK":
       return {
         ...state,
-        userPw: action.value,
+        password: action.value,
       };
 
     case "ADD_ID_ERROR_MSG":
+      // if (action.isError) {
       return {
         ...state,
-        idErrorMsg: action.message,
+        isError: action.isError,
+        errorMsg: action.message,
+        // };
       };
 
     case "ADD_PW_ERROR_MSG":

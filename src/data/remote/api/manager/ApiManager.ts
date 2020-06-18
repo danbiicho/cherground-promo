44bc06df59c10b95ea284c7a8cac9c3c24d1ab3d
danbi <danbiicho@gmail.com>
2020-06-18 15:25:49 +0900

    login API

--- src/data/remote/api/manager/ApiManager.ts
+++ src/data/remote/api/manager/ApiManager.ts
@@ -7,7 +7,7 @@ import axios, {
 
 export const BASE_URL =
   "https://c7f2aivpfg.execute-api.ap-northeast-2.amazonaws.com/dev";
-// https://j7zgha84s6.execute-api.ap-northeast-2.amazonaws.com/dev
+//https://blqrigt9nd.execute-api.ap-northeast-2.amazonaws.com/dev
 
 export const URL_USER = "sign-in";
 export const URL_SIGNUP = "sign-up";
